b31ca52420270baab882f54bf83d18aadf67b333
hasakii42 <63420887+hasakii42@users.noreply.github.com>
2022-04-13 18:27:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,161 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="escalation-v2screen">
+    <div class="overlap-group4">
+      <img class="line-145" src="./image/line.png" />
+      <img class="line-146" src="./image/line.png" />
+      <div class="group-1431">
+        <div class="overlap-group-container">
+          <div class="overlap-group1">
+            <div class="wedroboto-medium-black-16px">WED</div>
+            <div class="julroboto-medium-black-16px-2">Jul</div>
+          </div>
+
+          <div class="overlap-group">
+            <div class="numberroboto-medium-white-15px">30</div>
+          </div>
+        </div>
+        <div class="task-updates">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col">
+            <p class="new-task-assigned-to-you">New task assigned to you</p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-1">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-1">
+            <p class="new-feedback-recieved-for-task-nameroboto-normal-black-16px">New feedback recieved for Task Name</p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-2">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-2">
+            <p class="task-marked-completed-by-youroboto-normal-black-16px">Task marked completed by you</p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-3">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-3">
+            <p class="task-name-updated-foroboto-normal-black-16px">Task Name updated for journey Journey Name</p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-4">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-4">
+            <p class="follow-up-on-feedbacroboto-normal-black-16px">
+              Follow up on feedback for Task Name completed on Date XX/YY/ZZZZ
+            </p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="group-1432">
+        <div class="overlap-group-container-1">
+          <div class="overlap-group2">
+            <div class="thuroboto-medium-black-16px">
+              <span class="span0roboto-medium-black-16px">TH</span><span class="span1roboto-medium-black-16px">U</span>
+            </div>
+            <div class="jul-1roboto-medium-black-16px-2">Jul</div>
+          </div>
+          <div class="overlap-group3">
+            <div class="numberroboto-medium-white-15px">29</div>
+          </div>
+        </div>
+
+        <div class="task-updates-5">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-5">
+            <div class="meeting-with-bobby-charlieroboto-normal-black-16px">Meeting with Bobby Charlie</div>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-6">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-6">
+            <div class="chat-about-design-mentorshiproboto-normal-black-16px">Chat about Design Mentorship</div>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-7">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-7">
+            <p class="critical-delay-obserroboto-normal-black-16px">Critical delay observed in time line adherence</p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="task-updates-8">
+          <div class="x1130-amroboto-medium-black-14px">11:30 AM</div>
+          <div class="flex-col-8">
+            <p class="finish-project-documroboto-normal-black-16px">Finish project documentation for review (1-1 checkin)</p>
+            <div class="flex-row">
+              <img class="photo" src="./image/photo.png" />
+              <div class="nameroboto-medium-black-14px">Sheila O’Riley</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="ellipse-400"></div>
+      <div class="ellipse-401"></div>
+      <div class="ellipse-402"></div>
+      <div class="ellipse-403"></div>
+      <div class="ellipse-404"></div>
+      <div class="ellipse-405"></div>
+      <div class="ellipse-406"></div>
+      <div class="ellipse-407"></div>
+      <div class="ellipse-408"></div>
+
+      <img class="bxbxs-user-account" src="./image/account.png" />
+      <img
+        class="fluentperson-feedback-16-filled"
+        src="./image/talk.png"/>
+      <img class="akar-iconscheck" src="./image/tick.png" />
+      <img class="icoutline-browser-updated" src="./image/download.png"/>
+        <img class="richat-follow-up-line" src="./image/follow.png" />
+      <img class="logosgoogle-meet" src="./image/google.png" />
+      <img class="claritychat-bubble-outline-badged" src="./image/chat.png" />
+      <img class="fluentimportant-12-filled" src="./image/mark.png"/>
+        <img class="ggsand-clock" src="./image/clock.png" />
     </div>
+  </div>
   );
 }
 
